263e2dd1ccee467d729e4147f8e68401b23a2449
Nattapat <nattapat013@outlook.com>
2026-06-18 22:03:44 +0700

    Revert "feat: Add additional CloudWatch log groups for analyst, enrichment, and screener Lambda functions"
    This reverts commit b73b9e54a95a411fa7af2b4b99cf01483be3f07e.

--- terraform/cloudwatch.tf
+++ terraform/cloudwatch.tf
@@ -7,18 +7,3 @@ resource "aws_cloudwatch_log_group" "notification_lambda_log_group" {
   name              = "/aws/lambda/${aws_lambda_function.notification_function.function_name}"
   retention_in_days = 1
 }
-
-resource "aws_cloudwatch_log_group" "analyst_lambda_log_group" {
-  name              = "/aws/lambda/${aws_lambda_function.analyst_function.function_name}"
-  retention_in_days = 1
-}
-
-resource "aws_cloudwatch_log_group" "enrichment_lambda_log_group" {
-  name              = "/aws/lambda/${aws_lambda_function.enrichment_function.function_name}"
-  retention_in_days = 1
-}
-
-resource "aws_cloudwatch_log_group" "screener_lambda_log_group" {
-  name              = "/aws/lambda/${aws_lambda_function.screener_function.function_name}"
-  retention_in_days = 1
-}
